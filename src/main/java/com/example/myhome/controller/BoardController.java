93afffaa9e83ac07789d8479ee6e9f5231f0c4e2
Kimjunghyunn <93319948+Kimjunghyunn@users.noreply.github.com>
2022-03-26 23:37:51 +0900

    JPA를 이용한 RESTful API 작성, 페이지 처리 및 검색

--- src/main/java/com/example/myhome/controller/BoardController.java
+++ src/main/java/com/example/myhome/controller/BoardController.java
@@ -4,14 +4,18 @@ import com.example.myhome.model.Board;
 import com.example.myhome.repository.BoardRepository;
 import com.example.myhome.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
-import java.util.List;
-import java.util.Optional;
 
 @Controller
 @RequestMapping("/board")
@@ -24,8 +28,15 @@ public class BoardController {
     private BoardValidator boardValidator;
 
     @GetMapping("/list")
-    public String list(Model model) {
-        List<Board> boards = boardRepository.findAll();
+    public String list(Model model, @PageableDefault(size = 2) Pageable pageable,
+                       @RequestParam(required = false, defaultValue = "") String searchText) {
+        //Page<Board> boards = boardRepository.findAll(pageable);
+        Page<Board> boards = boardRepository.findByTitleContainingOrContentContaining(searchText, searchText, pageable);
+        int startPage = Math.max(1, boards.getPageable().getPageNumber() - 4);
+        int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() + 4);
+
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
         model.addAttribute("boards", boards);
         return "board/list";
     }
--- src/main/java/com/example/myhome/repository/BoardRepository.java
+++ src/main/java/com/example/myhome/repository/BoardRepository.java
@@ -1,8 +1,21 @@
 package com.example.myhome.repository;
 
 import com.example.myhome.model.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    //제목을 이용하여 찾는 인터페이스
+    List<Board> findByTitle(String title);
+
+    //제목 or 내용을 이용하여 찾는 인터페이스
+    List<Board> findByTitleOrContent(String title, String content);
+
+    Page<Board> findByTitleContainingOrContentContaining(String title, String content, Pageable pageable);
+
 
 }
