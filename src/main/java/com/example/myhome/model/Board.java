6d9159f09e06bf27122b583c7dde8aaab88587e2
Kimjunghyunn <93319948+Kimjunghyunn@users.noreply.github.com>
2022-03-29 02:27:38 +0900

    @OneToMany 관계 설정

--- src/main/java/com/example/myhome/model/Board.java
+++ src/main/java/com/example/myhome/model/Board.java
@@ -1,11 +1,9 @@
 package com.example.myhome.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -22,4 +20,10 @@ public class Board {
     @Size(min=2, max=30, message = "제목의 길이는 2자 이상 30자 이하이어야 합니다.")
     private String title;
     private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonIgnore
+    private User user;
+
 }
